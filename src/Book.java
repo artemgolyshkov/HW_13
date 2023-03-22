5b3567c83fd727d9940d8ff30c3790b1bdb6e3cb
Артем <artemgolyshkov@mail.ru>
2023-03-22 21:05:30 +0300

    Домашка №13 переделал.

--- src/Book.java
+++ src/Book.java
@@ -1,14 +1,20 @@
 public class Book {
    private String nameBook;
+   private Author author;
    private int yearBook;
 
-    public Book(String nameBook, int yearBook){
+
+    public Book(String nameBook, Author author, int yearBook){
         this.nameBook = nameBook;
+        this.author = author;
         this.yearBook = yearBook;
 
     }
      public String getNameBook(){
         return this.nameBook;
+     }
+     public Author getAuthor(){
+        return this.author;
      }
      public int getYearBook(){
         return this.yearBook;
--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,8 @@ public class Main {
     public static void main(String[] args) {
         Author alexPushkin = new Author("Александр ","Пушкин");
         Author levTolstoy = new Author("Лев ", "Толстой");
-        Book evgeniyOnegin = new Book("Евгений Онегин ", 1835);
-        Book  warAndPeace = new Book("Война и мир ",1868);
+        Book evgeniyOnegin = new Book("Евгений Онегин ",alexPushkin,1835);
+        Book  warAndPeace = new Book("Война и мир ",levTolstoy,1868);
         System.out.println("Автор - " + alexPushkin.getNameAuthor() + alexPushkin.getSurNameAuthor() + "; Книга - " + evgeniyOnegin.getNameBook() + evgeniyOnegin.getYearBook() + " год");
         System.out.println("Автор - " + levTolstoy.getNameAuthor() + levTolstoy.getSurNameAuthor()  + "; Книга - " + warAndPeace.getNameBook() + warAndPeace.getYearBook() + " год" );
         evgeniyOnegin.setYearBook(1833);
